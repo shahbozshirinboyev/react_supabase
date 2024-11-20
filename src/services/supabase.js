f720e48b8a9f9a787a92a2f8d043a6d00b78f3e4
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-20 17:26:53 +0500

    config.js from supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://riouxjduebosieuvqpuv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpb3V4amR1ZWJvc2lldXZxcHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwOTI3ODAsImV4cCI6MjA0NzY2ODc4MH0.MebPC1E0aFBMbrhxm3yq67uyJ_HAvUtNfylBaOyuOt8"
-);
+const apiUrl = "https://riouxjduebosieuvqpuv.supabase.co";
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpb3V4amR1ZWJvc2lldXZxcHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwOTI3ODAsImV4cCI6MjA0NzY2ODc4MH0.MebPC1E0aFBMbrhxm3yq67uyJ_HAvUtNfylBaOyuOt8";
+
+export const supabase = createClient(apiUrl, apiKey);
